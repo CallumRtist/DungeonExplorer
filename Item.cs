4faef556d4632d0556fc31e94e70f07c1a560d7d
CallumRtist <callumretallic@gmail.com>
2025-03-11 19:11:01 +0000

    Created Item Class
    Created item class with appropriate properties and constructor.

---
+++ Item.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DungeonExplorer
+{
+    public class Item
+    {
+        public String Name;
+
+        public int Value;
+
+        public int Quantity;
+
+        // Item Class Attributes
+        public Item(string name, int value, int quantity = 0)
+        {
+            Name = name;
+            Value = value;
+            Quantity = quantity;
+        }
+    }
+}
