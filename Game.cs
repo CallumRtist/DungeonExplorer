d890b9bf70dd0db9eed957a2ae3f9d8e8065d466
CallumRtist <callumretallic@gmail.com>
2025-03-12 15:23:04 +0000

    Implemented suggested changes from reviews
    Implemented name checks to prevent whitespace and numbers in name.
    Implemented randomised items in each room.
    Implemented use of lowercase characters in start and actions.
    Added item as room property.

--- Game.cs
+++ Game.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Media;
 using System.Runtime.InteropServices;
 using System.Security.Policy;
@@ -12,6 +13,8 @@ namespace DungeonExplorer
         private Room currentRoom;
         private List<string> roomList;
         private List<Item> itemList;
+        private List<Item> randomItems;
+        private Random randomClass = new Random();
 
         public Game()
         {
@@ -24,9 +27,17 @@ namespace DungeonExplorer
             Item moneyItem = new Item("Bag of Money", 25);
             Item chestItem = new Item("Treasure Chest", 1000);
 
-            // Create list of all items
+            // Create list of all items & a list of all items which can be randomly found in each room
             itemList = new List<Item>() { healthItem, moneyItem, chestItem };
+            randomItems = new List<Item>() { healthItem, moneyItem, null };
 
+        }
+
+        // When called on, generate an interger from 0 - 2 and use to select an item from the randomItems list (generate a random item)
+        public Item GenerateItem()
+        {
+            int randomItem = randomClass.Next(3);
+            return randomItems[randomItem];
         }
 
         public void Start()
@@ -38,22 +49,33 @@ namespace DungeonExplorer
             while (playing == false)
             {
                 Console.WriteLine("\nWelcome to Dungeon Explorer \nType S to Start");
-                string start = Console.ReadLine();
-                if (start == "S")
+                string start = Console.ReadLine().ToLower();
+                if (start == "s")
                 {
                     playing = true;
                 }
             }
 
-            // Setting Player Name and Health
+            // Setting player Name, prevents the player from entering nothing, spaces and/or numbers & special characters
             string playerName = "";
 
             while (playerName == "")
             {
                 Console.WriteLine("\nEnter a name:");
                 playerName = Console.ReadLine();
+                if (playerName.Any(char.IsWhiteSpace))
+                {
+                    playerName = "";
+                    Console.WriteLine("\nName cannot contain spaces");
+                }
+                if (playerName.All(char.IsLetter) == false)
+                {
+                    playerName = "";
+                    Console.WriteLine("\nName cannot contain numbers or special characters");
+                }
             }
 
+            // Setting Player Name, Health and Money
             int playerHealth = 10;
 
             player = new Player(playerName, playerHealth, 0);
@@ -61,14 +83,14 @@ namespace DungeonExplorer
             // Loop while Playing Boolean is true
             while (playing)
             {
-                currentRoom = new Room("Starter Room");
+                currentRoom = new Room("Starter Room", GenerateItem());
 
                 // Loop for each Room Name in roomList until reaching final room (get Room name)
                 foreach (string roomDesc in roomList)
                 {
                     bool hasSearched = false;
                     bool inRoom = true;
-                    currentRoom = new Room(roomDesc);
+                    currentRoom = new Room(roomDesc, GenerateItem());
                     Console.WriteLine($"\nYou find yourself within the {currentRoom.GetDescription()}");
 
                     // Loop until actions are completed within current room
@@ -77,18 +99,18 @@ namespace DungeonExplorer
 
                         
                         Console.WriteLine("What would you like to do? \n\nType C to check your current stats/room type \nType E to search for items \nType \"Use *Item Name*\" to use an item \nType W to go to the next room");
-                        string action = Console.ReadLine();
+                        string action = Console.ReadLine().ToLower();
 
                         // Check the room description, Player health/money/inventory and name
-                        if (action == "C")
+                        if (action == "c")
                         {
                             Console.WriteLine($"\nThe current room is the {currentRoom.GetDescription()}");
                             player.PlayerStats();
                             player.InventoryContents();
                             Console.WriteLine("\n");
                         }
-                        // Search the Room for items, return message if already searched
-                        if (action == "E")
+                        // Search the Room for items, get a random item from randomItem list, return message if room already searched
+                        if (action == "e")
                         {
                             if (hasSearched == true)
                             {
@@ -97,31 +119,19 @@ namespace DungeonExplorer
 
                             if (hasSearched == false)
                             {
-                                // Will give Health Item in the 1st, 4th and 7th rooms
-                                if (roomDesc == "Starter Room" || roomDesc == "Fourth Room" || roomDesc == "Seventh Room")
-                                {
-                                    Console.WriteLine("\nYou searched the room and found... ");
-                                    Console.WriteLine("...a Health Potion!");
-                                    player.PickUpItem(itemList[0]);
-                                    hasSearched = true;
-                                }
+                                Console.WriteLine("\nYou searched the room and found... ");
 
-                                // Will give nothing in the 2nd, 5th and 8th rooms
-                                if (roomDesc == "Second Room" || roomDesc == "Fifth Room" || roomDesc == "Eighth Room")
+                                if (currentRoom.Item != null)
                                 {
-                                    Console.WriteLine("\nYou searched the room and found... ");
+                                    Console.WriteLine($"...a {currentRoom.Item.Name}!");
+                                    player.PickUpItem(currentRoom.Item);
+                                }
+                                else
+                                {
                                     Console.WriteLine("...nothing");
-                                    hasSearched = true;
                                 }
 
-                                // Will give Money Item in the 3rd, 6th and 9th rooms
-                                if (roomDesc == "Third Room" || roomDesc == "Sixth Room" || roomDesc == "Ninth Room")
-                                {
-                                    Console.WriteLine("\nYou searched the room and found... ");
-                                    Console.WriteLine("...a Bag of Money!");
-                                    player.PickUpItem(itemList[1]);
-                                    hasSearched = true;
-                                }
+                                hasSearched = true;
 
                                 // Will give Treasure Item in the Final Room
                                 if (roomDesc == "Final Room")
@@ -135,19 +145,19 @@ namespace DungeonExplorer
                         }
 
                         // Use Item
-                        if (action == "Use Health Potion")
+                        if (action == "use health potion")
                         {
                             player.Health += itemList[0].Value;
                             Console.WriteLine("\nYou used the Health Postion and gained 5 health!\n");
                             player.RemoveItem(itemList[0]);
                         }
-                        if (action == "Use Bag of Money")
+                        if (action == "use bag of money")
                         {
                             player.Money += itemList[1].Value;
                             Console.WriteLine("\nYou opened the Bag of Money and got 25 coins!\n");
                             player.RemoveItem(itemList[1]);
                         }
-                        if (action == "Use Treasure Chest")
+                        if (action == "use treasure chest")
                         {
                             player.Money += itemList[2].Value;
                             Console.WriteLine("\nYou opened the Treasure Chest and found 1000 coins!!!\n");
@@ -155,7 +165,7 @@ namespace DungeonExplorer
                         }
 
                         // Go to the next Room, if Final Room then quit game by setting Playing Boolean to false
-                        if (action == "W")
+                        if (action == "w")
                         {
                             if (roomDesc == "Final Room")
                             {
